3947f5e14c69cb4a42fccd36eddbff3cdb591236
dnyaneshwar <dsn.navale@gmail.com>
2023-04-10 16:14:55 +0530

    upto 3 section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//function App() { // function declartion this way ... below is second way for declare function
+const App= () =>{   // another way to write function
+  const expenses=[
+    {id: 'e1', title: 'Toilet Paper',amount:94.12, date: new Date(2021,1,10)},
+    {id: 'e2', title: 'New TV',amount:940.12, date: new Date(2022,2,10)},
+    {id: 'e3', title: 'Car Insurance',amount:5000.12, date: new Date(2022,3,10)},
+    {id: 'e4', title: 'New Desk (wooden)',amount:300.12, date: new Date(2023,4,10)},
+
+  ];
+ 
+  //return (
+  //  <div>
+    //  <h2> Let's het started</h2>
+    // <Expenses items= {expenses} />
+    //</div>
+  //);
+
+  // alternative way - write in React just pass 3 element to createElement - create one main element & other are child element
+
+  return React.createElement(
+    'div',{},  // div 
+    React.createElement('h2',{},"Let\'s het started") , // h2 element
+    React.createElement(Expenses, {items: expenses}) // expenses class
+    );
 }
 
 export default App;
